c27d2b0e942128b2d56653aee9bd28b5063da5e8
Prashanth sistla <55555671+PSSJJ0198@users.noreply.github.com>
2020-06-24 23:00:29 +0530

    Add files via upload

---
+++ homepage.js
@@ -0,0 +1,22 @@
+import React, { Component } from 'react';
+import Header from './header';
+import Footer from './footer';
+import Body from './homebody';
+import './app.css';
+import { render } from '@testing-library/react';
+
+class Home extends Component{
+
+  render(){
+    return (
+      <div className='app'>
+        <Header></Header>
+        <Body></Body>
+        <Footer></Footer>
+      </div>
+    );
+    }
+  }
+export default Home;
+
+
