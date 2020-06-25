007e7e5ee369e603377d9dd4cdc40045396957dd
Prashanth sistla <55555671+PSSJJ0198@users.noreply.github.com>
2020-06-25 21:55:23 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -12,6 +12,7 @@ class App extends Component{
     return (
       /*<Router>
        <Switch>
+        <Route exact path="/" component={Home} />
         <Route exact path="/homepage.js" component={Home} />
         <Route exact path="/loginpage" component={Login} />
         <Route exact path="/usrlogin" component={ULogin} />
