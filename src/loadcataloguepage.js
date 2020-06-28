725e0d7daa2c059e6fb867ec9709c7534531c595
Prashanth sistla <55555671+PSSJJ0198@users.noreply.github.com>
2020-06-28 14:34:24 +0530

    Add files via upload

---
+++ src/loadcataloguepage.js
@@ -0,0 +1,20 @@
+
+import React, { Component } from 'react';
+import CataloguePage from './cataloguePage';
+
+
+ class Loadcatalogue extends Component{
+
+    render() {
+            return(
+                    
+                    <CataloguePage></CataloguePage>
+            );
+        
+      
+      
+      }
+}
+  
+ export default Loadcatalogue;
+  
