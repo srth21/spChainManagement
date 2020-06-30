7a9b9ecc801fcbe2dad7638a5db48287c6886a36
Sai Rohit <sasistla@microsoft.com>
2020-06-30 23:26:43 +0530

    Updates

---
+++ precache-manifest.bf2bd2defe36a4d95d773bbf2ce55a07.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4060c5ed81f7eaf3e6a24374b9ead2a0",
+    "url": "/spChainManagement/index.html"
+  },
+  {
+    "revision": "2ed37d6fa1186423336c",
+    "url": "/spChainManagement/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "9cc557dbd7e795d9906e",
+    "url": "/spChainManagement/static/css/main.e45cb647.chunk.css"
+  },
+  {
+    "revision": "2ed37d6fa1186423336c",
+    "url": "/spChainManagement/static/js/2.f92f43b4.chunk.js"
+  },
+  {
+    "revision": "eca27f575f87cbd558dfc6ef3fe9fcab",
+    "url": "/spChainManagement/static/js/2.f92f43b4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9cc557dbd7e795d9906e",
+    "url": "/spChainManagement/static/js/main.be23cb8e.chunk.js"
+  },
+  {
+    "revision": "f7d101bf291746245e34",
+    "url": "/spChainManagement/static/js/runtime-main.02fdb8b3.js"
+  },
+  {
+    "revision": "5982f9952a13d8a1f5d8b65b8312bb4c",
+    "url": "/spChainManagement/static/media/iron.5982f995.jpg"
+  },
+  {
+    "revision": "4f7b035f23c5169e421875607d3256a9",
+    "url": "/spChainManagement/static/media/medium.4f7b035f.jpg"
+  },
+  {
+    "revision": "5c8d7914c8422947e770f290eb372fc2",
+    "url": "/spChainManagement/static/media/micro.5c8d7914.jpg"
+  },
+  {
+    "revision": "dce9cade7c2b4acb6158b02b67d63466",
+    "url": "/spChainManagement/static/media/small.dce9cade.webp"
+  }
+]);
